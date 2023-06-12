0566bfe51ea5f6b3ffb4f41807a77910156f726b
BennyKowalsky <102220075+BennyKowalsky@users.noreply.github.com>
2023-06-12 16:05:34 +0200

    Add files via upload

---
+++ Website/scriptBoss.js
@@ -0,0 +1,9 @@
+const button = document.getElementById("button");
+
+
+button.addEventListener('click', () => {
+  window.scrollBy({
+    top: window.innerHeight,
+    behavior: 'smooth'}) });
+
+    document.getElementById("vido").src="../";
