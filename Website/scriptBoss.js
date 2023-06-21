b9917de81eca5b17ca58aecf44cd483c5ccb73df
BennyKowalsky <102220075+BennyKowalsky@users.noreply.github.com>
2023-06-21 23:23:47 +0200

    Delete Website directory

--- Website/scriptBoss.js
+++
@@ -1,9 +0,0 @@
-const button = document.getElementById("button");
-
-
-button.addEventListener('click', () => {
-  window.scrollBy({
-    top: window.innerHeight,
-    behavior: 'smooth'}) });
-
-    document.getElementById("vido").src="../";
